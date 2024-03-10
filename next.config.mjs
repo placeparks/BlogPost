cb4a520bdacbc57996b38738e5bebf34393a8853
placeparks <86215469+placeparks@users.noreply.github.com>
2024-03-11 02:59:49 +0500

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,11 @@ const nextConfig = {
   experimental: {
     serverActions: true,
   },
+      env: {
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+    GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+    GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
+      },
   };
   
   export default nextConfig;
